1c2f9285cda8c836d4ee72d8ddcaacc8f7da6218
Jacek <Jacek@BERLIN>
2020-02-21 14:37:41 +0100

    dsad

--- spring-demo-annotation-field-injection/src/pl/jaceksysiak/springdemo/TennisCoach.java
+++ spring-demo-annotation-field-injection/src/pl/jaceksysiak/springdemo/TennisCoach.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 @Component
 public class TennisCoach implements Coach {
 
+	//injecting dependency by a field
 	@Autowired
 	private FortuneService fortuneService;
 
